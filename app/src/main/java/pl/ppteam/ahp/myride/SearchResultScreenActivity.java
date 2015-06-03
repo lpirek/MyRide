88c0a898bc24877cbda1febce5a24e78fbbb27ea
Piotr Płaczek <piotr.placzek@wp.pl>
2015-06-03 18:20:40 +0200

    img

--- app/src/main/java/pl/ppteam/ahp/myride/SearchResultScreenActivity.java
+++ app/src/main/java/pl/ppteam/ahp/myride/SearchResultScreenActivity.java
@@ -12,6 +12,7 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Toast;
+import android.view.MenuInflater;
 
 import java.util.List;
 
@@ -82,25 +83,19 @@ public class SearchResultScreenActivity extends ActionBarActivity implements Vie
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        //getMenuInflater().inflate(R.menu.menu_search_result_screen, menu);
-        //return true;
-        return false;
+        getMenuInflater().inflate(R.menu.main_add_ride, menu);
+        return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.action_add_ride:
+                addRide();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 
 
@@ -113,6 +108,12 @@ public class SearchResultScreenActivity extends ActionBarActivity implements Vie
             default:
                 Logger.info("Unkown action source!");
         }
+    }
+
+    private void addRide()
+    {
+
+
     }
 
     private void chooseRide() {
